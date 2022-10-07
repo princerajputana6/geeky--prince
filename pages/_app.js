774348fe34f94dfc3c8048891da2359d60c56577
Prince RajPut <rincerajputana6@gmail.com>
2022-10-08 05:13:12 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 
+import React from "react";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
